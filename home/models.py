e2f5ae1f0776440459a0533b04c5766b91423c92
wololokingdoms <pablo.rpm@gmail.com>
2019-06-07 19:35:13 -0300

    models_101_created

--- home/models.py
+++ home/models.py
@@ -1,7 +1,46 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+    #
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('body', classname="full")
+    # ]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    attributes = models.ManyToManyField('Attribute', blank=True)
+
+
+class Service(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    premium = models.BooleanField(null=True)
+    attributes = models.ManyToManyField('Attribute', blank=True)
+
+    def getAttributeValues(self):
+        return AttributeValue.objects.filter(service=self)
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=50)
+    categories = models.ManyToManyField('Category', through=Category.attributes.through, blank=True)
+    services = models.ManyToManyField('Service', through=Service.attributes.through, blank=True)
+
+
+class AttributeValue(models.Model):
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    value = models.CharField(max_length=50)
+
+
+class Voucher(models.Model):
+    type = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
