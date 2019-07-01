8b476a43d1963e3819c2b8b35864f138d18e47db
wololokingdoms <pablo.rpm@gmail.com>
2019-07-01 18:08:38 -0300

    added slate theme

--- home/models/pages/ServicePage.py
+++ home/models/pages/ServicePage.py
@@ -1,7 +1,7 @@
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
-from ..business_models import Service
+from ..business_models import Attribute
 
 
 class ServicePage(Page):
@@ -10,9 +10,11 @@ class ServicePage(Page):
     def get_context(self, request):
         context = super(ServicePage, self).get_context(request)
         context['table_rows'] = []
-        for service in Service.objects.all():
-            attrs = service.getAttributes()
-            attrs['name'] = service.name
+        for attribute in Attribute.objects.all():
+            attrs = attribute.section
             context['table_rows'].append(attrs)
         return context
 
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full")
+    ]
