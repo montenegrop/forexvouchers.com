b73b14147c14b00a08df1144047c75f0f86e408c
Matias Montenegro <matias@analyticsfire.com>
2019-07-12 16:00:06 -0300

    flags svg https://github.com/lipis/flag-icon-css

--- forexvouchers/settings/base.py
+++ forexvouchers/settings/base.py
@@ -145,7 +145,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static')
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
