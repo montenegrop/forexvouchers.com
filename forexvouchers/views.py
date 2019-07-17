31417a9b676fb54237e501ef47cba5a7c0d347fe
pablo montenegro <pablo.rpm@gmail.com>
2019-07-17 12:03:24 -0300

    font changes

--- forexvouchers/views.py
+++ forexvouchers/views.py
@@ -16,7 +16,6 @@ def savecomments(request):
     g = GeoIP2()
     try:
         ip = request.META.get('REMOTE_ADDR')
-        # ip = '45.116.232.20'
         comment.country = g.city(ip)['country_name']
         comment.country_code = g.city(ip)['country_code']
         print(comment.country_code)
