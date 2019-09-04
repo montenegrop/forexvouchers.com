9a5c33294b86943e21c318fbbbfeb236a93dbac4
Matias Montenegro <matias@analyticsfire.com>
2019-09-04 09:14:54 -0300

    deployment scripts separated

---
+++ scripts/django-update.sh
@@ -0,0 +1,13 @@
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 source /usr/local/bin/virtualenvwrapper.sh
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 workon fxvouchers
+cd /var/www/fxvouchers
+
+export DJANGO_SETTINGS_MODULE=forexvouchers.settings.production
+FILE=/home/ubuntu/env && test -f $FILE && source $FILE
+
+
+pip install -r requirements.txt
+npm install
+npm run build:static
+python manage.py migrate
+sudo supervisorctl restart all
--- scripts/git-update.sh
+++ scripts/git-update.sh
@@ -1,14 +1,6 @@
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 source /usr/local/bin/virtualenvwrapper.sh
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3 workon fxvouchers
 cd /var/www/fxvouchers
 git fetch origin master
 git checkout --force master
 git reset --hard origin/master
-FILE=/home/ubuntu/env && test -f $FILE && source $FILE
 
-
-pip install -r requirements.txt
-npm install
-npm run build:static
-DJANGO_SETTINGS_MODULE=forexvouchers.settings.production python manage.py migrate
-sudo supervisorctl restart all
+source ./django-update.sh
