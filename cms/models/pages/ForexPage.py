d6d3d20070af5cc432be76e46bd62fbbb64d730e
Matias Montenegro <matias@analyticsfire.com>
2020-01-28 23:35:00 -0300

    Forex pages

--- cms/models/pages/ForexPage.py
+++ cms/models/pages/ForexPage.py
@@ -1,19 +1,19 @@
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from django.db.models import TextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtailschemaorg.models import PageLDMixin
 from wagtailschemaorg.utils import extend
 
 class ForexPage(RoutablePageMixin, PageLDMixin, Page):
-    body = RichTextField(blank=True)
+    script = TextField(blank=True)
 
     def get_context(self, request):
         context = super(ForexPage, self).get_context(request)
         return context
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('script', classname="full")
     ]
 
     def ld_entity(self):
