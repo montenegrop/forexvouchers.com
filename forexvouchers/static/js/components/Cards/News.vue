9c3041fd73e0b512a6f4f06078fbfebfc73abc0c
pablo montenegro <pablo.rpm@gmail.com>
2020-03-02 03:30:31 -0300

    news styling font changed

--- forexvouchers/static/js/components/Cards/News.vue
+++ forexvouchers/static/js/components/Cards/News.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container partner-card text-center">
-        <div class="text-center">
-            <a class="mb-0 text-primary" :href="`${entry.link}`" target="_blank"><h6 class="text-left no-shadow font-weight-bold">{{ entry.title }}</h6></a>
+        <div class="text-center mt-1">
+            <a class="mb-0 text-primary mt-1" :href="`${entry.link}`" target="_blank"><h6 class="text-left no-shadow mb-1">{{ entry.title }}</h6></a>
             <p class="text-left mb-1">Read more on <a class="text-primary" target="_blank" :href="entry.source.url_site">{{
                 entry.source.url_site }}</a></p>
         </div>
