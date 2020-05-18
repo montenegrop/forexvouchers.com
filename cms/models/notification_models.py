6f5b1c39217c5dbbd35d6348227cbf64d0407e91
pablo montenegro <pablo.rpm@gmail.com>
2020-05-17 23:57:24 -0300

    removed intro from contact form

--- cms/models/notification_models.py
+++ cms/models/notification_models.py
@@ -71,14 +71,12 @@ class ContactPage(WagtailCaptchaEmailForm, PageLDMixin):
             'name': 'Forex Vouchers',
         })
 
-    intro = RichTextField(blank=True)
     Thank_you_box = models.CharField(max_length=300, blank=True, default=None, null=True)
     Thank_you_follow = models.CharField(max_length=150, blank=True, default=None, null=True)
     Thank_you_mail = models.CharField(max_length=100, blank=True, default=None, null=True)
 
     content_panels = AbstractEmailForm.content_panels + [
         FieldPanel('title'),
-        FieldPanel('intro'),
         InlinePanel('form_fields', label='Form Fields'),
         FieldPanel('Thank_you_box'),
         FieldPanel('Thank_you_follow'),
