16ee6ed0e1fc5c14efae04b7b09994adf76c810e
pablo montenegro <pablo.rpm@gmail.com>
2020-06-07 23:23:52 -0300

    remoded ShowLikes from vouchers in Home-popular-vouchers

--- forexvouchers/static/js/components/HomePage/HomeVouchers.vue
+++ forexvouchers/static/js/components/HomePage/HomeVouchers.vue
@@ -1,7 +1,8 @@
 <template>
     <b-row>
         <b-col cols="12" md="4" v-for="voucher in vouchers" class="service-summary pb-3">
-            <fv-voucher :key="voucher.id" :voucher="voucher" :show-clicks="true" :is-flex="isFlex"></fv-voucher>
+            <fv-voucher :key="voucher.id" :voucher="voucher" :show-clicks="true" :is-flex="isFlex"
+                        :show-likes="false"></fv-voucher>
         </b-col>
     </b-row>
 </template>
