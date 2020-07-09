ca96dd28da676897e51a194e435c23c8b9c9bc88
pablo montenegro <pablo.rpm@gmail.com>
2020-07-09 03:19:26 -0300

    discount remaketing

--- forexvouchers/static/js/components/Vouchers/Middleware/discount.vue
+++ forexvouchers/static/js/components/Vouchers/Middleware/discount.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="container middleware-main">
-        <h2 class="title"> {{ voucher.type }} information </h2>
+        <h2 class="title"> {{ voucher.name }}</h2>
         <div class="row border border-light no-float ">
-            <div class="col-auto image">
-                <img :src="voucher.logo">
 
-                <b-col class="col-auto product-stars rate-container flexible voucher-info-service pl-3">
-
-                    <rate :length="5" :value="voucher.service_rate" readonly/>
-                </b-col>
-            </div>
             <div class="col information">
-                <div class="row  product-info-discount name">
-                    <h3>{{ voucher.name }}</h3>
-                    <h4 class="discount">{{ voucher.discount_percent }}% OFF</h4>
+
+                <div class="col-auto image text-align-center mt-0">
+                    <img :src="voucher.logo">
+
+                    <p class="text-dark font-weight-bold mb-0">By: <a class="product-service-link"
+                                                                      :href="voucher.service_url">{{
+                        voucher.service_name }}</a></p>
+
+                    <b-col class="col-auto product-stars rate-container flexible voucher-info-service pl-3">
+
+                        <rate :length="5" :value="voucher.service_rate" readonly/>
+                    </b-col>
+                </div>
+
+                <div class="row  product-info-discount name justify-content-end">
+                    <h4 class="discount mr-3">{{ voucher.discount_percent }}% OFF</h4>
                 </div>
                 <div class="row  description" v-html="voucher.description"></div>
-                <div class="row  icons">
-
+                <div class="row  icons justify-content-center">
                     <p v-if="voucher.never_expires" class="icons">
                         <i class="fas fa-hourglass-half"></i> Never Expires
                     </p>
-
-
                     <p v-else class="icons"><i class="fas fa-hourglass-half"></i>
                         Expires in:&nbsp;
                         <timeago :datetime="voucher.expires"></timeago>
                     </p>
-
                 </div>
                 <div class="row button button-container">
                     <div class=" button-border">
@@ -39,28 +41,27 @@
                     </div>
                 </div>
 
-                <div class="product-service">
-                    <p>
-                        <i class="fas fa-external-link-alt"></i>
-                        Go <a class="product-service-link"
-                              :href="voucher.service_url"><u>{{ voucher.service_name }}</u> </a> in-site page
-                    </p>
+                <div class="product-service row">
+                    <b-col>
+                        <p><i class="fas fa-external-link-square-alt"></i>
+                            <a class="product-service-link"
+                               :href="voucher.service_affiliate.url"></a> website
+                        </p>
 
-                    <p><i class="fas fa-external-link-square-alt"></i>
-                        External <a class="product-service-link"
-                                    :href="voucher.service_affiliate.url"><u>{{ voucher.service_name }}</u> </a> website
-                    </p>
-
-                    <p><i class="fas fa-eye"></i>
-                        {{ voucher.affiliate.clicks }} Views
-                    </p>
-                        <Likes classes="btn btn-sm" :likes="voucher.likes" :dislikes="voucher.dislikes" :id="voucher.id"></Likes>
+                        <p><i class="fas fa-eye"></i>
+                            {{ voucher.affiliate.clicks }} Views
+                        </p>
+                    </b-col>
+                    <b-col class="text-right">
+                        <Likes classes="btn btn-sm" :likes="voucher.likes" :dislikes="voucher.dislikes"
+                               :id="voucher.id"></Likes>
+                    </b-col>
                 </div>
             </div>
         </div>
 
         <div class="mt-2 mb-5">
-            <h5 class="title text-left mb-0 title-more-vouchers">Related Coupons:</h5>
+            <h5 class="title text-left mb-0 title-more-vouchers">Related vouchers:</h5>
             <fv-vouchers-home class="VueCarousel-inner-margin" sort="mostviewed"></fv-vouchers-home>
 
             <div class="show-more right">
